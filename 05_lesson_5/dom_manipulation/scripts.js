beb563b484df6ad05e70a02e7b273f6fa616112a
jonatantroche <alm19004@byui.edu>
2022-10-17 17:13:30 -0400

    solve empty list problem

--- 05_lesson_5/dom_manipulation/scripts.js
+++ 05_lesson_5/dom_manipulation/scripts.js
@@ -1,24 +1,23 @@
 const list = document.querySelector('ul');
-      const input = document.querySelector('input');
-      const button = document.querySelector('button');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-      button.addEventListener('click', () => {
-        const myItem = input.value;
-        input.value = '';
-
-        const listItem = document.createElement('li');
-        const listText = document.createElement('span');
-        const listBtn = document.createElement('button');
-
-        listItem.appendChild(listText);
-        listText.textContent = myItem;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = '❌';
-        list.appendChild(listItem);
-
-        listBtn.addEventListener('click', () => {
-          list.removeChild(listItem);
-        });
-
-        input.focus();
-      });
+  button.addEventListener('click', () => {
+    const myItem = input.value;
+    if ( myItem !== "") {
+      input.value = ''
+      const listItem = document.createElement('li');
+      const listText = document.createElement('span');
+      const listBtn = document.createElement('button');
+    
+      listItem.appendChild(listText);
+      listText.textContent = myItem;
+      listItem.appendChild(listBtn);
+      listBtn.textContent = '❌';
+      list.appendChild(listItem)
+      listBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+      })
+      input.focus();
+    }
+  });
