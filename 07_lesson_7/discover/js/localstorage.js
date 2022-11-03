8f32a030a42f3a23a0c5d83f83ebc317aa6216f6
jonatantroche <alm19004@byui.edu>
2022-11-02 23:32:30 -0400

    localstoage solution

--- 07_lesson_7/discover/js/localstorage.js
+++ 07_lesson_7/discover/js/localstorage.js
@@ -1,13 +1,18 @@
 const visit = document.querySelector('#visit');
 
 window.addEventListener("load", () => {
-    let date = new Date();
-    let save = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
-    if (localStorage.getItem("lastDate") == null) {
-    localStorage.setItem("lastDate", save);
-console.log("Last date: " + localStorage.getItem("lastDate"));
-} else {
-console.log("Last date: " + localStorage.getItem("lastDate"));
-}
+    let oldDate = localStorage.getItem("lastDate"); 
+    let currentDate = Date.now();
+    if ( oldDate === null || oldDate === ""){
+        oldDate = currentDate;
+    }
+    
+    // let oldDate = 1467442058802;
+    // let convertedDate = new Date(oldDate);
+    let days = Math.trunc((currentDate - oldDate) / 86400000);
+  
+    
+    localStorage.setItem("lastDate", currentDate);
 
+    visit.innerHTML = days;
 });
