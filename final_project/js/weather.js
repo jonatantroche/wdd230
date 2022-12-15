378c68b6bb61996557b2b7af4236366e9d7c5498
jonatantroche <alm19004@byui.edu>
2022-12-15 00:54:24 -0400

    forecast update

--- final_project/js/weather.js
+++ final_project/js/weather.js
@@ -3,7 +3,7 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const humidity = document.querySelector('#humidity');
 const tomorrowTemp = document.querySelector('#day_1');
-const url_1 = "https://api.openweathermap.org/data/2.5/weather?id=5334223&units=imperial&appid=57d16f210f6741a79b3b085f67258384";
+const tomorrowTemp1 = document.querySelector('#day_2');
 const url = "https://api.openweathermap.org/data/2.5/forecast?id=5334223&appid=57d16f210f6741a79b3b085f67258384";
 
 //calling weather data from url
@@ -23,42 +23,26 @@ async function apiFetch() {
 }
 apiFetch();
 
-
-async function apiFetch1() {
-    try {
-    const response_1 = await fetch(url_1);
-    if (response_1.ok) {
-        const data1= await response_1.json();
-        console.log(data1); // this is for testing the call
-        displayResults(data1);
-    } else { 
-        throw Error(await response_1.text());
-    }
-    } catch (error) {
-        console.log(error);
-    }
-}
-apiFetch1();
-
 //display data 
 function  displayResults(weatherData) {
-    let tempN = weatherData.main.temp.toFixed(0);
+    let tempN = weatherData.list[0].main.temp.toFixed(0);
     currentTemp.innerHTML = tempN;
-    humidity.innerHTML = `${weatherData.main.humidity.toFixed(0)}`; 
-    
+    let humidity_data = weatherData.list[0].main.humidity.toFixed(0); 
+    humidity.innerHTML = humidity_data 
+    let text = weatherData.list[0].weather[0].description;
+    captionDesc.innerHTML = text;
+
+//forecast
+    let day_1 = weatherData.list[8].main.temp.toFixed(0);
+    tomorrowTemp.innerHTML = day_1;
+
+    let day_2 = weatherData.list[16].main.temp.toFixed(0);
+    tomorrowTemp1.innerHTML = day_2;
 
 //icon 
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    const desc = weatherData.weather[0].description;
-
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc.toUpperCase(); //weather information in uppercase
-
+   const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`;
+   weatherIcon.setAttribute('src', iconsrc);
+   weatherIcon.setAttribute('alt', desc);
+   captionDesc.textContent = desc.toUpperCase(); //weather information in uppercase
     
 }
-
-// 3 days forecast
-//function  displayResults(weatherData) {
-//    day_1.innerHTML = `${weatherData.list[4].main.temp.toFixed(0)}`;
-//}
